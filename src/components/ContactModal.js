0b89f3920d6ca63de91dfbe999c28d2ed19baccf
Jade Swift <jadeswift@Jades-MacBook-Pro.local>
2021-11-22 14:10:14 -0600

    form take 700

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -1,10 +1,58 @@
 import Button from './Button';
 import Rodal from 'rodal';
-import React, { useState } from 'react';
 
 const ContactModal = props => {
   const [visible, setVisible] = useState(false);
-  let pathname = typeof window !== "undefined" ? window.location.pathname : ""
+  const [submitted, setSubmitted] = useState(false);
+  const [submitterName, setSubmitterName] = useState("");
+  let pathname = typeof window !== 'undefined' ? window.location.pathname : '';
+
+  const form = (
+    <form
+      method='POST'
+      name='contact-form'
+      className='contactForm'
+      data-netlify='true'>
+
+      <input
+        name='form-name'
+        value='contact-form'
+        type='hidden' />
+
+      <input
+        type="hidden"
+        name="subject"
+        value={`You've got mail from ${submitterName}`} />
+
+      <input
+        type='text'
+        name='name'
+        required={true}
+        onChange={(e) => setSubmitterName(e.target.value)}
+        placeholder='Enter your name' />
+
+      <input
+        type='email'
+        name='email'
+        required={true}
+        placeholder='Enter your email' />
+
+      <textarea
+        name='message'
+        required={true}
+        placeholder='Message' />
+
+      <Button action={() => setSubmitted(true)} type='submit'>Submit</Button>
+
+    </form>
+  );
+
+  const submittedSuccess = (
+    <div className={"flex items-center flex-col"}>
+      <p className={"text-lg text-primary pt-32 pb-8"}>Submission Successful</p>
+      <Button action={() => setVisible(false) & setSubmitted(false)}>Close Window</Button>
+    </div>
+  );
 
   return (
     <div>
@@ -15,47 +63,17 @@ const ContactModal = props => {
         leaveAnimation='zoom'
         closeOnEsc
         visible={visible}
-        onClose={() => setVisible(false)}
+        onClose={() => setVisible(false) & setSubmitted(false)}
         dis
         height={425}
         width={pathname.innerWidth < 500 ? pathname.innerWidth - 5 : 400}
       >
-        <form
-          method='POST'
-          name='contactform'
-          className='contactForm'
-          data-netlify="true">
-
-          <input
-            name="form-name"
-            value="contactform"
-            type="hidden" />
-
-          <input
-            type='text'
-            name='name'
-            placeholder='Enter your name' />
-
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter your email' />
-
-          <textarea
-            name='message'
-            placeholder='Message' />
-
-          <Button action={() => setVisible(false)} type='submit'>Submit</Button>
-
-        </form>
-        <form name='contactForm' netlify hidden>
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <textarea name='message' />
-        </form>
+        {submitted ? submittedSuccess : form}
       </Rodal>
     </div>
-  )
-}
+  );
+};
 
-export default ContactModal
+import React, { useState } from 'react';
+
+export default ContactModal;
