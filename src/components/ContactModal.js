05ea9ebf4f0da70e3544e89573c151d72345b4a4
Jade Swift <jadeswift@Jades-MacBook-Pro.local>
2021-11-24 10:57:14 -0600

    the submit button should work..

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -53,31 +53,17 @@ const ContactModal = (props) => {
                 setVisible(false);
               });
           }}
-          validate={values => {
-            const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-            const errors = {};
-            if (!values.name) {
-              errors.name = 'Name Required';
-            }
-            if (!values.email || !emailRegex.test(values.email)) {
-              errors.email = 'Valid Email Required';
-            }
-            if (!values.message) {
-              errors.message = 'Message Required';
-            }
-            return errors;
-          }}
         >
           {({ errors, touched }) => (
             <Form id='form' name='contact-form' data-netlify={true}>
-              <label className='hidden' htmlFor='name'> </label>
-              <Field id='form-field' placeholder='Enter Name' name='name' className={touched.name && errors.name ? 'error-field' : ''} />
+              <label className='hidden' htmlFor='name'>Name: </label>
+              <Field id='form-field' placeholder='Enter Name' name='name' />
 
-              <label className='hidden' htmlFor='email'> </label>
-              <Field id='form-field' placeholder='Enter Email' name='email' className={touched.email && errors.email ? 'error-field' : ''} />
+              <label className='hidden' htmlFor='email'>Email: </label>
+              <Field id='form-field' placeholder='Enter Email' name='email' />
 
-              <label className='hidden' htmlFor='message'><ErrorMessage name={'message'} /></label>
-              <Field id='form-textarea' placeholder='Enter Message' name='message' component='textarea' className={touched.message && errors.message ? 'error-field' : ''} />
+              <label className='hidden' htmlFor='message'>Message: </label>
+              <Field id='form-textarea' placeholder='Enter Message' name='message' component='textarea' />
 
               <Button type='submit'>Send</Button>
             </Form>
